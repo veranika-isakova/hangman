f407fc7d945ba6cad87a0fb38f6b2c1c58870c5f
VERANIKA <veranika.isakova89@gmail.com>
2017-12-03 14:10:47 +0100

    Change Reducer, add function showGuess, add status to defaultState

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,5 +1,4 @@
 import React, {PureComponent} from 'react'
-import toMarkdown from 'to-markdown'
 import { connect } from 'react-redux'
 import guessLetter  from '../actions/letters/guess'
 //import PropTypes from 'prop-types'
@@ -12,42 +11,38 @@ class Game extends PureComponent {
   constructor(props) { //initialize in Ruby, constructor of class Game
     super() //constructor of PureComponent
 
-    this.state = { //local state
+    this.state = { //local state of Game
       letter: ''
     }
   }
-
-  updateLetter(event) {
-    this.setState({
+  
+  updateLetter(event) { //onChange accepts event as a parameter
+    this.setState({ // setState is from PureComponent
       letter:this.refs.letter.value
     })
   }
 
   tryLetter() {
     console.table(this.state)
-    const letter = {
-      ...this.state,
-      letter: toMarkdown(this.state.letter)
-    }
-    console.table(letter)
-    this.props.save(letter)
+    this.props.save(this.state.letter);
+    this.setState({ //clean input
+      letter: ''
+    })
   }
 
 	render() { // call every time when something is changing
-    const { word_to_show, guesses } = this.props
+    const { word_to_show, guesses, status } = this.props
 
 		return (
 			 <div>
           <p>{ word_to_show }</p>
           <p>{ guesses }</p>
-
+          <p>{ status }</p>
             Your guess: <input
             type="text"
             ref="letter"
-            className="letter"
-            defaultValue={this.state.letter}
-            onChange={this.updateLetter.bind(this)}
-            onKeyUp={this.updateLetter.bind(this)} />
+            value={this.state.letter}
+            onChange={this.updateLetter.bind(this)} />
 
           <div className="actions">
             <button className="primary" onClick={this.tryLetter.bind(this)}>Try</button>
@@ -60,9 +55,10 @@ class Game extends PureComponent {
 const mapStateToProps = ({ letters }) => (
   {
     word_to_show: letters.word_to_show,
-    guesses: letters.guesses
+    guesses: letters.guesses,
+    status: letters.status
   }); //1) letters (from reducer). 2) this.props (here)
 
-const mapDispatchToProps = { save: guessLetter }
+const mapDispatchToProps = { save: guessLetter } //save - function, any name, use in tryLetter
 
-export default connect(mapStateToProps, mapDispatchToProps)(Game)
+export default connect(mapStateToProps, mapDispatchToProps)(Game)// connect component to redux
--- src/reducers/letters.js
+++ src/reducers/letters.js
@@ -3,16 +3,40 @@ import { GUESS_LETTER } from '../actions/letters/guess'
 const defaultState = {
   word_to_show: '_ _ _ _ _ _',
   word_to_guess: 'Winter',
-  guesses: []
+  guesses: [],
+  status: ''
+}
+// step2
+function toNewLetter(letter, guesses) {
+  if (guesses.includes(letter)) {
+    return letter
+  } else {
+    return "_"
+  }
+}
+function showGuess(word, guesses) {
+  // take word
+  //split word into letters
+  //take guesses
+  // if first letter isn't in our array guesses we put "_"
+  // or put our letter
+  //["h","e", "l", "l", "o"]
+  //["_","_","l","l","_"]
+  var letter_word = word.split("")
+  var new_letter_word = letter_word.map((old_letter) => toNewLetter(old_letter, guesses))
+
+  return new_letter_word.join(" ")
 }
 
 export default (state = defaultState, {type, payload} = {}) => {
   switch(type) {
-    case GUESS_LETTER :
-      //return [{ ...payload, _id: newId(state) }].concat(state)
-        //return word_to_show
-      //})
-      return state;
+    case GUESS_LETTER:
+
+      var new_guesses = state.guesses.concat(payload.toLowerCase()); // add new guess to guesses
+      var new_word_to_show = showGuess(state.word_to_guess.toLowerCase(), new_guesses);
+
+      return {...state, word_to_show: new_word_to_show, guesses: new_guesses}// status: new_status
+
     default :
       return state
   }
